84e0f47c21420e533feb19003630ecef997a693b
Giancarlo Fabrizi <gfabrizi@Giancarlos-Air.lan>
2016-07-06 17:51:35 +0200

    bike = docking_station.release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,9 @@
 require 'docking_station'
 describe DockingStation do
-  it "Releases a bike" do should respond_to(:release_bike) end
+  it { is_expected.to respond_to :release_bike}
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
