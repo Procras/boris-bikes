45a5d247bddcf7bbc0b2822b64f72f7d4a4497e6
Giancarlo Fabrizi <gfabrizi@Giancarlos-Air.lan>
2016-07-07 17:08:42 +0200

    exercise 12(raise error)

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,24 +1,25 @@
 require 'docking_station'
-describe DockingStation do
-  it { is_expected.to respond_to(:dock).with(1).argument }
-  it { is_expected.to respond_to(:bike)}
+  describe DockingStation do
+    it { is_expected.to respond_to(:dock).with(1).argument }
+    it { is_expected.to respond_to(:bike)}
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+
+
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
   end
 
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
   end
 
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+  describe '#release_bike' do
+      it 'raises an error when there are no bikes' do
+
+      expect {subject.release_bike}.to raise_error "No bikes available"
+    end
   end
-
-
-
 end
